eec9d39319bb03be1c5b41c5acb0fec4d0cd34aa
patrick ballantyne <P.J.Ballantyne@liverpool.ac.uk>
2021-09-22 15:05:50 +0100

    updates to method

--- Source Code/Helper Functions.R
+++ Source Code/Helper Functions.R
@@ -74,5 +74,82 @@ get_walking_isolines <- function(x) {
   return(iso)
 }
   
+## Function for delineating walking catchments for the retail centres
+get_boundary_walking <- function(x) {
   
+  ## Create a 20m buffer from the boundary
+  rc_lines <- st_cast(x, "LINESTRING")
+  rc_buffer <- st_buffer(rc_lines, 20)
+  
+  ## Pull out the H3's for the retail centre
+  rc_h3 <- polyfill(x, 11, FALSE)
+  rc_h3 <- h3_to_polygon(unlist(rc_h3$h3_polyfillers), simple = FALSE)
+  rc_h3 <- st_transform(rc_h3, 27700)
+  
+  ## Get hexagons on the boundary
+  bound_h3 <- rc_h3[rc_buffer, op = st_intersects]
+  bound_cent <- st_centroid(bound_h3)
+  bound_cent <- bound_cent %>% select(geometry)
+  
+  ## Add centroid for rc
+  x_cent <- st_centroid(x)
+  x_cent <- x_cent %>% select(geom) %>% rename(geometry = geom)
+  centres <- st_as_sf(rbind(bound_cent, x_cent))
+  
+  ## Split
+  b_ls <- split(centres, seq(nrow(centres)))
+  
+  ## Function
+  getWalk <- function(x) {
+    
+    if(rc_lines$Classification == "Regional Centre" | rc_lines$Classification == "Major Town Centre" | rc_lines$Classification == "Large Shopping Centre" | rc_lines$Classification == "Town Centre") {
+      iso <- isoline(x, range = (15 * 60), range_type = "time", transport_mode = "pedestrian")
+      iso$RC_ID <- rc_lines$RC_ID
+      iso$RC_Name <- rc_lines$RC_Name
+      iso$Classification <- rc_lines$Classification
+      iso$Duration <- "15 mins"
+      iso <- iso[, c("RC_ID", "RC_Name", "Classification", "Duration")]
+      Sys.sleep(1)
+    }
+    if(rc_lines$Classification == "Large Retail Park" | rc_lines$Classification == "Small Retail Park" | rc_lines$Classification == "Small Shopping Centre") {
+      iso <- isoline(x, range = (10* 60), range_type = "time", transport_mode = "pedestrian")
+      iso$RC_ID <- rc_lines$RC_ID
+      iso$RC_Name <- rc_lines$RC_Name
+      iso$Classification <- rc_lines$Classification
+      iso$Duration <- "10 mins"
+      iso <- iso[, c("RC_ID", "RC_Name", "Classification", "Duration")]
+      Sys.sleep(1)
+    }
+    if(rc_lines$Classification == "District Centre" | rc_lines$Classification == "Market Town" | rc_lines$Classification == "Local Centre") {
+      iso <- isoline(x, range = (10 * 60), range_type = "time", transport_mode = "pedestrian")
+      iso$RC_ID <- rc_lines$RC_ID
+      iso$RC_Name <- rc_lines$RC_Name
+      iso$Classification <- rc_lines$Classification
+      iso$Duration <- "10 mins"
+      iso <- iso[, c("RC_ID", "RC_Name", "Classification", "Duration")]
+      Sys.sleep(1)
+    }
+    
+    return(iso)
+}
+  
+  ## Get the individual hexagon catchments
+  catch <- do.call(rbind, lapply(b_ls, getWalk))
+  catch_diss <- catch %>%
+    summarise()
+  catch <- catch %>%
+    as.data.frame() %>%
+    select(RC_ID, RC_Name, Classification, Duration) %>%
+    distinct()
+  catch_diss$RC_ID <- catch$RC_ID
+  catch_diss$RC_Name <- catch$RC_Name
+  catch_diss$Classification <- catch$Classification
+  catch_diss$Duration <- catch$Duration
+  
+  ## Remove any holes
+  catch_diss <- sfheaders::sf_remove_holes(catch_diss)
+  print(paste0(rc_lines$RC_Name, " ", "Completed"))
+  return(catch_diss)
+  
+}
 
